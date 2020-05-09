8ce1b2dfab66cd2b7ab3c87396a647bf6b8eb001
Jakob Willforss <jakob.willforss@hotmail.com>
2020-05-09 13:17:20 +0200

    Setup minimal framework

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import plotly.graph_objects as go
+fig = go.Figure()
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)
+
+
