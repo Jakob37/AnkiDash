75c6a6cc9bbe21074c444e9a7555823063b87a26
Jakob Willforss <jakob.willforss@hotmail.com>
2020-05-17 16:45:19 +0200

    Update target for output data, and setup hover scatter for overviewing all characters

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Exploring the Anki reviews using Python Dash
 
 See it running at [Heroku](https://anki-dash.herokuapp.com/)
+
+# How-to
+
+Access the SQL-database for Anki at
+
+~/.local/share/Anki2/User 1/collection.anki2
+
+Export the tables:
+
+* notes
+* cards
+* revlog
+
+Should probably script this one!
--- app.py
+++ app.py
@@ -19,6 +19,8 @@ fig = px.bar(
     y="reviews",
     title="Jakob's most reviewed characters")
 
+axis_choices = ['avg_ease', 'reviews', 'curr_dur']
+
 app.layout = html.Div(children=[
     html.H1('Anki Dash'),
     dcc.Slider(id='input_slider', min=1, max=100, value=10),
@@ -28,10 +30,20 @@ app.layout = html.Div(children=[
                  options=[{'label':i,'value':i} for i in nbr_revs_df.character],
                  multi=True,
                  placeholder='A placeholder..'),
-    dcc.Graph(id='charplot')
+    dcc.Graph(id='charplot'),
+    html.H3('Opacity'),
+    dcc.Slider(id='scatter_opacity', min=0, max=1, value=0.6, step=0.05),
+    html.H3('X-axis'),
+    dcc.Dropdown(id='scatter_xaxis', options=axis_choices, value='reviews'),
+    html.H3('Y-axis'),
+    dcc.Dropdown(id='scatter_yaxis', options=axis_choices, value='avg_ease'),
+    html.H3('Color'),
+    dcc.Dropdown(id='scatter_color', options=axis_choices, value='curr_dur'),
+    dcc.Graph(id='targethistplot')
     ]
 )
-# 一
+
+
 @app.callback(
     Output('mainplot', 'figure'),
     [Input('input_slider', 'value')]
@@ -40,6 +52,7 @@ def update_figure(show_count):
     filter_df = nbr_revs_df.head(show_count)
     my_fig = px.bar(filter_df, x="character", y="reviews", title="Jakob's most reviewed characters")
     return my_fig
+
 
 @app.callback(
     Output('charplot', 'figure'),
@@ -52,9 +65,35 @@ def character_graph(characters):
     else:
         revs_to_plot = all_revs_df[all_revs_df['character'] == characters]
 
-    my_fig = px.line(revs_to_plot, y="duration", color="character")
-    # my_fig.update_layout(yaxis_type="log")
+    my_fig = px.line(revs_to_plot, x="nbr", y="duration", color="character")
+    return my_fig
 
+
+@app.callback(
+    Output('targethistplot', 'figure'),
+    [
+        Input('scatter_opacity', 'value'),
+        Input('scatter_xaxis', 'value'),
+        Input('scatter_yaxis', 'value'),
+        Input('scatter_color', 'value')
+    ]
+)
+def target_hist(scatter_opacity, xaxis, yaxis, color):
+
+    #color_map = {"ease_1": "red", "ease_2": "gray", "ease_3": "green", "ease_4": "blue"}
+
+    my_fig = px.scatter(
+        nbr_revs_df,
+        x=xaxis,
+        y=yaxis,
+        color=color,
+        log_y=False,
+        color_continuous_scale=['red', 'lightgreen', 'blue'],
+        #color_discrete_map=color_map,
+        opacity=scatter_opacity,
+        hover_name="character",
+        title="Distribution of duration and review"
+    )
     return my_fig
 
 
